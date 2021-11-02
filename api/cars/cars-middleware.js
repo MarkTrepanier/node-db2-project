3f099e2893d3182acea863ac248b0004331e407d
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-11-02 17:08:36 -0400

    middleware, working on number validity

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,38 @@
+const Car = require("./cars-model");
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  Car.getById(req.params.id)
+    .then((car) => {
+      if (car) {
+        req.car = car;
+        next();
+      } else {
+        next({ status: 404, message: "car with id <car id> is not found" });
+      }
+    })
+    .catch(next);
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  const { vin, make, model, mileage } = req.body;
+  if (!vin || !make || !model || !mileage) {
+    next({ status: 400, message: "<field name> is missing" });
+  } else {
+    next();
+  }
+};
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
+};
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+};
